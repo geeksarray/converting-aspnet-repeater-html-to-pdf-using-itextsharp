ecb6849822723f7de4e2ae7e3637aa9da88bdc7b
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-08-13 20:48:01 +0530

    initial files

---
+++ NorthwindApp/NorthwindApp/CustomerExport.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Title="Exporting Customer details to PDF using iTextSharp by dotnetmentors.com"
+    Language="C#" MasterPageFile="~/Site.Master"  EnableEventValidation="false"   AutoEventWireup="true" CodeBehind="CustomerExport.aspx.cs"
+    Inherits="NorthwindApp.CustomerExport" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Repeater ID="repCustomers" runat="server">
+        <HeaderTemplate>
+            <table cellspacing="0" cellpadding="5" rules="all" border="1" id="gvDetails" style="border-collapse: collapse;">
+                <tr style="color: White; background-color: #DF5015; font-weight: bold;">
+                    <th scope="col">
+                        CustomerID
+                    </th>
+                    <th>
+                        CompanyName
+                    </th>
+                    <th>
+                        ContactName
+                    </th>
+                    <th>
+                        ContactTitle
+                    </th>
+                    <th>
+                        Address
+                    </th>
+                    <th>
+                        City
+                    </th>
+                    <th>
+                        Country
+                    </th>
+                </tr>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <tr>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).CustomerID%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).CompanyName%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).ContactName%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).ContactTitle%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).Address%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).City%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).Country%>
+                </td>
+            </tr>
+        </ItemTemplate>
+        <AlternatingItemTemplate>
+            <tr>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).CustomerID%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).CompanyName%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).ContactName%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).ContactTitle%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).Address%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).City%>
+                </td>
+                <td>
+                    <%# ((NorthwindApp.Customer)Container.DataItem).Country%>
+                </td>
+            </tr>
+        </AlternatingItemTemplate>
+        <FooterTemplate>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+    <br />
+    <table>
+        <tr style="width: 100%">
+            <td style="padding-left: 300px">
+                <asp:PlaceHolder ID="plcPaging" runat="server" />
+            </td>
+        </tr>
+    </table>
+    <asp:Button ID="btnPDF" runat="server" Text="Export to PDF"   OnClick="btnPDF_Click" />
+    <asp:Button ID="btnExportAllCustomer" runat="server" 
+        Text="Export All Customers to PDF" onclick="btnExportAllCustomer_Click" 
+         />
+    <asp:Button ID="btnExportPage" runat="server" 
+        Text="Export web page" onclick="btnExportPage_Click" 
+         />    
+</asp:Content>
---
+++ NorthwindApp/NorthwindApp/CustomerExport.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+
+namespace NorthwindApp
+{
+    public partial class CustomerExport : System.Web.UI.Page
+    {
+
+        private const int PageSize = 10;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            plcPaging.Controls.Clear();
+            CreatePagingControl();
+            if (!Page.IsPostBack)
+            {
+                FetchData(1);
+            }
+        }
+
+        private void CreatePagingControl()
+        {
+            int rowCount = CustomerService.GetCustomerCount();
+            rowCount = (rowCount / PageSize) + 1;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                LinkButton lnk = new LinkButton();
+                lnk.Click += new EventHandler(lbl_Click);
+                lnk.ID = "lnkPage" + (i + 1).ToString();
+                lnk.Text = (i + 1).ToString();
+                plcPaging.Controls.Add(lnk);
+                Label spacer = new Label();
+                spacer.Text = "&nbsp;";
+                plcPaging.Controls.Add(spacer);
+            }
+        }
+
+
+        void lbl_Click(object sender, EventArgs e)
+        {
+            LinkButton lnk = sender as LinkButton;
+            int currentPage = int.Parse(lnk.Text);
+
+            FetchData(currentPage);
+        }
+
+        private void FetchData(int pageIndex)
+        {
+            repCustomers.DataSource = CustomerService.GetAllCustomers(pageIndex - 1, PageSize);
+            repCustomers.DataBind();
+        }
+
+        protected void btnPDF_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=Customers.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+            
+            //this.Page.RenderControl(hw);            
+            this.repCustomers.RenderControl(hw);
+            StringReader sr = new StringReader(sw.ToString().Replace("\r", "").Replace("\n", "").Replace("  ", ""));
+            Document pdfDoc = new Document(iTextSharp.text.PageSize .A4, 10f, 10f, 10f, 0.0f);
+
+            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+            PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+            pdfDoc.Open();
+            htmlparser.Parse(sr);
+            pdfDoc.Close();
+            Response.Write(pdfDoc);
+            Response.End();
+        }       
+
+        protected void btnExportAllCustomer_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=Customers.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+
+            Repeater repAllCustomers = this.repCustomers;
+            repAllCustomers.DataSource = CustomerService.GetAllCustomers(0, CustomerService.GetCustomerCount());
+            repAllCustomers.DataBind();
+            repAllCustomers.RenderControl(hw); 
+            
+            StringReader sr = new StringReader(sw.ToString().Replace("\r", "").Replace("\n", "").Replace("  ", ""));
+            Document pdfDoc = new Document(iTextSharp.text.PageSize.A4, 10f, 10f, 10f, 0.0f);
+
+            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+            PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+            pdfDoc.Open();
+            htmlparser.Parse(sr);
+            pdfDoc.Close();
+            Response.Write(pdfDoc);
+            Response.End();
+        }
+
+        protected void btnExportPage_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=Customers.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+
+            this.Page.RenderControl(hw);            
+            
+            StringReader sr = new StringReader(sw.ToString().Replace("\r", "").Replace("\n", "").Replace("  ", ""));
+            Document pdfDoc = new Document(iTextSharp.text.PageSize.A4, 10f, 10f, 10f, 0.0f);
+
+            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+            PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+            pdfDoc.Open();
+            htmlparser.Parse(sr);
+            pdfDoc.Close();
+            Response.Write(pdfDoc);
+            Response.End();
+        }
+    }
+}
